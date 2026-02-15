91b659d7508d5a5b5723bc1fe420e0b6fa8b3c7d
Alexander Edemalm <edemalm.alexander@gmail.com>
2026-02-15 22:16:57 +0100

    feat(logger): Logger, styling, formating, prints e.g. INFO, ERROR

---
+++ src/utils/logger.py
@@ -0,0 +1,48 @@
+import datetime
+from typing import NamedTuple
+from src.ui.style import Style
+
+class LogStyle(NamedTuple):
+    label: str
+    color: str
+
+    def __str__(self):
+        """Override, ToString"""
+        return f"{self.color}[{self.label}]{Style.ENDC}"
+
+class Logger:
+
+    class _STYLES:
+        """Internal storage, single truth of styles"""
+        INFO    = LogStyle("INFO",    Style.BLUE)
+        SUCCESS = LogStyle("SUCCESS", Style.GREEN)
+        WARNING = LogStyle("WARNING", Style.YELLOW)
+        ERROR   = LogStyle("ERROR",   Style.RED)
+
+    @staticmethod
+    def _is_show_date(is_date: bool) -> str:
+        """Internal helper, builds timestamp"""
+
+        if not is_date: return ""
+        
+        return f"[{datetime.datetime.now().strftime("%H:%M:%S")}]"
+    
+    @staticmethod
+    def _log(header: LogStyle, body: str, is_date: bool):
+        """Internal helper, assembles print message"""
+
+        is_date = Logger._is_show_date(is_date)
+
+        print(f"{is_date}{header} {body}")
+    
+    @staticmethod
+    def info(msg: str, d: bool = True): Logger._log(Logger._STYLES.INFO, msg, d)
+        
+    @staticmethod
+    def success(msg: str, d: bool = True): Logger._log(Logger._STYLES.SUCCESS, msg, d)
+
+    @staticmethod
+    def warning(msg: str, d: bool = True): Logger._log(Logger._STYLES.WARNING, msg, d)
+
+    @staticmethod
+    def error(msg: str, d: bool = True): Logger._log(Logger._STYLES.ERROR, msg, d)
